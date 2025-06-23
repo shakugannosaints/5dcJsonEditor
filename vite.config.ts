d90b9f8b0854d7dd68ffaca3366f9f11c0aec7aa
冷筱华 <43056969+shakugannosaints@users.noreply.github.com>
2025-06-23 19:22:50 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: '/5dcJsonEditor/', 
   plugins: [react()],
   resolve: {
     alias: {
